f26033c731bb82cb7f21ba4777868ca57f85a8a1
ihadfish <67808984+ihadfish@users.noreply.github.com>
2020-07-03 22:24:37 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,53 @@
 # bot.py
 import os
-
 import discord
+import json
 from dotenv import load_dotenv
+from discord.ext import commands
+from googletrans import Translator
 
+#LOADING BOT TOKEN AND SERVER ID
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+#BOT PREFIX IS '!'
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'{bot.user.name} has connected to Discord!')
 
+#BOT LEAVE/JOIN VOICE CHANNEL
+@bot.command(name = 'join')
+async def join(ctx):
+    toJoin = ctx.message.author.voice.channel
+    await toJoin.connect()
 
-    print(f'{client.user} has connected to the following guild:\n'
-          f'{guild.name}(id: {guild.id})')
+@bot.command(name = 'leave')
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
 
-client.run(TOKEN)
+#BOT TRANSLATOR
+class Translations(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.translator = Translator()
+
+    @commands.command(name = 'trans')
+    async def trans(self, ctx, mes, dest='english'):
+        translated = self.translator.translate(mes, dest)
+        reply = 'Original Language: ' + translated.src + '\nTranslated: ' + translated.text + '\nPronunciation: ' + translated.pronunciation
+        await ctx.send(reply)
+
+#TODO
+class Bank(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+#TODO
+class Gambling(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+bot.add_cog(Translations(bot))
+bot.run(TOKEN)
