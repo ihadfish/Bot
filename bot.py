ffd69b0debc191798766d443040e0a933704794d
Matthew Wu <67808984+mwu-dev@users.noreply.github.com>
2020-07-03 22:41:33 -0400

    Update bot.py
    f strings on translate

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ class Translations(commands.Cog):
     @commands.command(name = 'trans')
     async def trans(self, ctx, mes, dest='english'):
         translated = self.translator.translate(mes, dest)
-        reply = 'Original Language: ' + translated.src + '\nTranslated: ' + translated.text + '\nPronunciation: ' + translated.pronunciation
+        reply = f'Original Language: {translated.src}\nTranslated: {translated.text}\nPronunciation: {translated.pronunciation}'
         await ctx.send(reply)
 
 #TODO
