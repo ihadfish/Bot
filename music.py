274b4f0ec8711dcf3aaec6e9ee6bef0437b01d20
sbrenner132 <samuelbrenner@brandeis.edu>
2020-07-09 20:43:45 -0400

    optimizing text search for song, formatting with embeds

--- music.py
+++ music.py
@@ -1,19 +1,13 @@
 import discord
 import youtube_dl
 import os
-import re
-import asyncio
 from mutagen.mp3 import MP3
 from discord.ext import commands
 import urllib.request
 import urllib.parse
-import requests
 import json
 import urllib
-import pprint
 import re
-from bs4 import BeautifulSoup
-from youtube_search import YoutubeSearch
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -24,6 +18,7 @@ ydl_opts = {
     }],
 }
 
+
 class Player:
     def __init__(self):
         super().__init__()
@@ -31,13 +26,13 @@ class Player:
         self.curr_song = None
         self.curr_url = None
         self.song_file = None
-    
+
     def get_song_queue(self):
         return self.song_queue
-    
+
     def enqueue(self, url):
         self.song_queue.append(url)
-    
+
     def dequeue(self):
         return self.song_queue.pop(0)
 
@@ -52,26 +47,42 @@ class Player:
 
     def get_curr_url(self):
         return self.curr_url
-    
+
     def set_song_file(self, file):
         self.song_file = file
 
     def get_song_file(self):
         return self.song_file
 
+    def clear(self):
+        self.song_file = None
+        self.curr_url = None
+        self.curr_song = None
+        self.song_queue = []
+
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.players = {} # to be used for multiple server compatability
+        self.players = {}  # to be used for multiple server compatability
 
     @commands.command(name='play', pass_context=True)
     async def play(self, ctx, *, url=None):
         if ctx.voice_client is None:
-            await ctx.send(f'<@{ctx.author.id}> I am not in a voice channel, use .join to add me so we can start jamming!')
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> I am not in a voice channel, use .join to add me so we can start jamming!',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
             return
         elif url is None:
-            await ctx.send(f'<@{ctx.author.id}> please provide a song for me to play!')
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> please provide a song for me to play!',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
             return
         elif not self.check_url(url):
 
@@ -80,17 +91,23 @@ class Music(commands.Cog):
             uri = "https://www.youtube.com/results?search_query=" + query
             response = urllib.request.urlopen(uri)
             html = response.read()
-            soup = BeautifulSoup(html, 'html.parser')
-            search_results = re.findall(r'/watch\?v=(.{11})', str(soup))
-            
-            url = f'https://www.youtube.com/watch?v={search_results[0]}'
+
+            # this is causing problems with song playing
+            search_results = re.search(r'/watch\?v=(.{11})', str(str(html)))
+
+            url = f'https://www.youtube.com{search_results[0]}'
 
             # not returning after this - for now, use first search result as url
 
         player = self.get_player(ctx)
 
         async def queue():
-            await ctx.send(f'Adding song to queue: {self.song_title(url)}')
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> has added a song to queue: {self.song_title(url)}',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
             player.enqueue(url)
 
         if player.get_curr_song() is not None or player.get_curr_url() is not None:
@@ -115,13 +132,13 @@ class Music(commands.Cog):
             ydl.download([url])
 
         for file in os.listdir('./'):
-            if file.__contains__(self.song_title(url).replace('/','_')) and file.endswith('.mp3'):
+            if file.__contains__(self.song_title(url).replace('/', '_').replace('\"', '\'')) and file.endswith('.mp3'):
                 player.set_song_file(file)
-                print('renamed file: ', player.get_song_file())
                 os.rename(file, str(ctx.message.guild) + '-song.mp3')
+                break
 
         voice.play(discord.FFmpegPCMAudio(str(ctx.message.guild) + '-song.mp3'),
-                    after=lambda e: self.next_song(ctx))
+                   after=lambda e: self.next_song(ctx))
 
         voice.source = discord.PCMVolumeTransformer(voice.source)
         voice.source.volume = 0.07
@@ -133,22 +150,34 @@ class Music(commands.Cog):
             length = self.song_length(ctx)
 
             embed = discord.Embed(title="Now Playing",
-                                description=player.get_curr_song(), color=0x00FFE7)
+                                  description=player.get_curr_song(), color=0x00FFE7)
             embed.add_field(name="Length", value=length, inline=False)
             embed.add_field(name="Link to song on YouTube",
                             value=player.get_curr_url(), inline=False)
 
             await ctx.send(embed=embed)
         else:
-            await ctx.send(f'<@{ctx.author.id}> oh no! There is no song currently playing. Please provide me with some tunes to pump!')
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> oh no! There is no song currently playing. Please provide me with some tunes to pump!',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
 
     @commands.command(name='queue', pass_context=True)
     async def queue(self, ctx):
         player = self.get_player(ctx)
-        embed = discord.Embed(title="Current queue", description='These songs are currently lined up to be played!', color=0x00FFE7)
+        description = 'These songs are currently lined up to be played!' if len(
+            player.song_queue) else 'There are no songs currently in queue!'
+        embed = discord.Embed(
+            title="Current queue",
+            description=description,
+            color=0x00FFE7
+        )
         num = 1
         for song_url in player.get_song_queue():
-            embed.add_field(name=str(num) + '. ' + self.song_title(song_url), value=('Link: ' + song_url), inline=False)
+            embed.add_field(name=str(num) + '. ' + self.song_title(song_url),
+                            value=('Link: ' + song_url), inline=False)
             num = num + 1
         await ctx.send(embed=embed)
 
@@ -156,9 +185,26 @@ class Music(commands.Cog):
     async def pause(self, ctx):
         player = self.get_player(ctx)
         if ctx.voice_client is None:
-            await ctx.send(f'I am not in a voice channel')
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> please summon me to your voice channel!',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
+        elif not ctx.voice_client.is_playing() or player.get_curr_song() is None:
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> how do you expect to pause me if I am not playing anything?? Use !resume before you pause, dummy',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
         else:
-            embed = discord.Embed(title=f"Song paused", description=(f'<@{ctx.author.id}> has paused: ' + player.get_curr_song()), color=0x00FFE7)
+            embed = discord.Embed(
+                title=f"Song paused",
+                description=(
+                    f'<@{ctx.author.id}> has paused: ' + player.get_curr_song()),
+                color=0x00FFE7
+            )
             await ctx.send(embed=embed)
             ctx.voice_client.pause()
 
@@ -166,9 +212,26 @@ class Music(commands.Cog):
     async def resume(self, ctx):
         player = self.get_player(ctx)
         if ctx.voice_client is None:
-            await ctx.send(f'<@{ctx.author.id}> please summon me to your voice channel!')
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> please summon me to your voice channel!',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
+        elif ctx.voice_client.is_playing() or player.get_curr_song() is None:
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> I cannot resume a song that is currently playing! please !pause the music in order to be able to resume it, dummy',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
         else:
-            embed = discord.Embed(title=f"Song resumed", description=(f'<@{ctx.author.id}> has resumed: ' + player.get_curr_song()), color=0x00FFE7)
+            embed = discord.Embed(
+                title=f"Song resumed",
+                description=(
+                    f'<@{ctx.author.id}> has resumed: ' + player.get_curr_song()),
+                color=0x00FFE7
+            )
             await ctx.send(embed=embed)
             ctx.voice_client.resume()
 
@@ -176,29 +239,53 @@ class Music(commands.Cog):
     async def stop(self, ctx, skipped=False):
         player = self.get_player(ctx)
         if ctx.voice_client is None:
-            await ctx.send(f'<@{ctx.author.id}> please summon me to your voice channel!')
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> please summon me to your voice channel!',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
+        elif not ctx.voice_client.is_playing() or player.get_curr_song() is None:
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> I am not playing any music to stop! U need to add some tunes in order to stop, silly.',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
         else:
             if not skipped:
-                embed = discord.Embed(title=f"Song stopped", description=(f'<@{ctx.author.id}> has stopped: ' + player.get_curr_song()), color=0x00FFE7)
+                player.clear()
+                ctx.voice_client.stop()
+                embed = discord.Embed(title=f"Music stopped", description=(
+                    f'<@{ctx.author.id}> has stopped all music, and the queue has been cleared.'), color=0x00FFE7)
                 await ctx.send(embed=embed)
-            ctx.voice_client.stop()
-            player.set_curr_song(None)
-            player.set_curr_url(None)
+            else:
+                ctx.voice_client.stop()
 
     @commands.command(name='skip', pass_context=True)
     async def skip(self, ctx):
         player = self.get_player(ctx)
         if ctx.voice_client is None:
-            await ctx.send(f'<@{ctx.author.id}> please summon me to your voice channel!')
+            embed = discord.Embed(
+                title='',
+                description=f'<@{ctx.author.id}> please summon me to your voice channel!',
+                color=0x00FFE7
+            )
+            await ctx.send(embed=embed)
         else:
             if len(player.get_song_queue()) > 0:
-                embed = discord.Embed(title=f"Song skipped", description=(f'<@{ctx.author.id}> has skipped: ' + player.get_curr_song()), color=0x00FFE7)
+                embed = discord.Embed(title=f"Song skipped", description=(
+                    f'<@{ctx.author.id}> has skipped: ' + player.get_curr_song()), color=0x00FFE7)
                 await self.stop(ctx, True)  # stop current song
                 await ctx.send(embed=embed)
             else:
                 await self.stop(ctx, True)  # stop current song
-                await ctx.send(f'<@{ctx.author.id}> oh no! I have no more songs lined up to play. Give me some tunes!')
-            
+                embed = discord.Embed(
+                    title='',
+                    description=f'<@{ctx.author.id}> oh no! I have no more songs lined up to play. Give me some tunes!',
+                    color=0x00FFE7
+                )
+                await ctx.send(embed=embed)
 
     def check_url(self, url):
         regex = re.compile(
@@ -246,24 +333,25 @@ class Music(commands.Cog):
                 ydl.download([url])
 
             for file in os.listdir('./'):
-                if file.__contains__(self.song_title(url).replace('/','_')) and file.endswith('.mp3'):
+                if file.__contains__(self.song_title(url).replace('/', '_')) and file.endswith('.mp3'):
                     player.set_song_file(file)
                     os.rename(file, str(ctx.message.guild) + '-song.mp3')
+                    break
 
             voice.play(discord.FFmpegPCMAudio(str(ctx.message.guild) + '-song.mp3'),
                        after=lambda e: self.next_song(ctx))
 
             voice.source = discord.PCMVolumeTransformer(voice.source)
             voice.source.volume = 0.07
-        
+
         else:
             player.set_curr_song(None)
             player.set_curr_url(None)
-    
+
     def song_length(self, ctx):
         length = MP3(str(ctx.message.guild) + '-song.mp3').info.length
-        return str(int(length//60)) + ":" + str(int(length % 60))
-    
+        return '{:02d}'.format(int(length//60)) + ":" + '{:02d}'.format(int(length % 60))
+
     def get_player(self, ctx):
         try:
             return self.players[ctx.message.guild]
