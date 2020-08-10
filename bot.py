39b228adb7ca8923e3271ab29bd9b5c1fabd92eb
vnewyen <67808918+vnewyen@users.noreply.github.com>
2020-08-09 23:08:50 -0400

    Add files via upload
    Added function when mentioning bot; bot will respond with prefix for commands

--- bot.py
+++ bot.py
@@ -1,54 +1,121 @@
 # bot.py
 import os
 import discord
-import pymongo
-import translation
-import bank
-from pymongo import MongoClient
+import json
+import datetime
 from dotenv import load_dotenv
-from discord.ext import commands, tasks
-from discord.ext.commands import CommandNotFound
+from discord.ext import commands
+from googletrans import Translator
 
 #LOADING BOT TOKEN AND SERVER ID
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-
-#LOAD CLIENT DATABASE FROM MONGODB
-client = pymongo.MongoClient(os.getenv('MONGODB_LINK'))
-db = client['BotDB']
 
 #BOT PREFIX IS '!'
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='i!')
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-#catches user commands that aren't found and prints to the console
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        print(f'<{ctx.author.display_name}> tried to execute a command: {ctx.message.content}, but there is no such command.')
-
 #BOT LEAVE/JOIN VOICE CHANNEL
 @bot.command(name = 'join')
 async def join(ctx):
-    if ctx.message.author.voice is None:
-        await ctx.send(f'<@{ctx.author.id}>, you are not in a voice channel for me to join.')
-        return
     toJoin = ctx.message.author.voice.channel
     await toJoin.connect()
 
 @bot.command(name = 'leave')
 async def leave(ctx):
-    if ctx.voice_client is None:
-        await ctx.send('I am not in a voice channel.')
-        return
     await ctx.voice_client.disconnect()
 
+#BOT TRANSLATOR
+class Translations(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.translator = Translator()
 
-bot.add_cog(translation.Translations(bot))
-bot.add_cog(bank.Bank(bot, db['Economy']))
-bot.add_cog(bank.Gambling(bot, db['Economy']))
+    @commands.command(name = 'trans')
+    async def trans(self, ctx, mes, dest='english'):
+        translated = self.translator.translate(mes, dest)
+
+        embedTrans = discord.Embed(
+            title = '',
+            description = '',
+            colour = discord.Colour.blue()
+        )
+
+        embedTrans.add_field(name = f'Original Language:', value = translated.src, inline = False)
+        embedTrans.add_field(name = f'Translated:', value = translated.text, inline = False)
+        embedTrans.add_field(name = f'Pronunciation:', value = translated.pronunciation, inline = False)
+        await ctx.send(embed = embedTrans)
+
+#SERVER INFO
+@bot.command(name = 'info')
+async def info(ctx):
+    embedInfo = discord.Embed(
+        title = f'Tower of Matt',
+        description = 'Long ago Matt was gay, he still is. <:emoji1:729889712239673344>',
+        colour = discord.Colour.blue()
+    )
+
+    embedInfo.add_field(name = f'Creation of Tower', value = ctx.guild.created_at, inline = False)
+    embedInfo.add_field(name = f'Number of Regulars <:khucie:460943559059832854>', value = ctx.guild.member_count, inline = False)
+    embedInfo.set_thumbnail(url = 'https://i.imgur.com/bhmaknw.jpeg')
+    embedInfo.set_image(url = 'https://i.imgur.com/bhmaknw.jpeg')
+
+    await ctx.send(embed = embedInfo)
+
+#MATT IS GAY command
+@bot.command(name = 'matt')
+@commands.cooldown(1, 86400, commands.BucketType.user)
+async def matt(ctx):
+        embedMatt = discord.Embed(
+        title = '',
+        description = 'Matt is gay!',
+        colour = discord.Colour.blue()
+        )
+
+        await ctx.send(embed = embedMatt)
+
+@matt.error
+async def matt_error(ctx, error):
+
+    t = str(datetime.timedelta(seconds = error.retry_after))
+    t = t.split('.')[0]
+    h, m, s = t.split(':')
+
+    t = str(datetime.timedelta(hours = int(h), minutes = int(m), seconds = int(s)))
+    h, m, s = t.split(':')
+
+    embedErrorOne = discord.Embed(
+        title = '',
+        description = f'I will not bully Matt, jk... try again in {h} hours {m} minutes {s} seconds.',
+        colour = discord.Colour.red()
+    )
+
+    embedErrorOne.set_footer(text = 'Try !kenton in the meantime!')
+
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(embed = embedErrorOne)
+    else:
+        raise error
+
+#BOT PREFIX
+@bot.event
+async def on_message(message):
+    #replace id with your bots ID
+    if message.content.startswith('<@!728896861225877534>'):
+        channel = message.channel
+        await channel.send(f'My Prefix is {bot.command_prefix}')
+
+#TODO
+class Bank(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+#TODO
+class Gambling(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+bot.add_cog(Translations(bot))
 bot.run(TOKEN)
